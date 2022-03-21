06d186269a11c89ac9e2a501b5f5f2fd1296a804
joshua <joshuadavidoni@yahoo.com>
2022-03-21 18:23:28 +0100

    test

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const signin = require('./controllers/signin');
 const image = require('./controllers/image');
 
 const db = knex({
+  client: pg,
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
