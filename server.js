f342c36a2c5d040037a92b9c49c4174e8ec9aea9
joshua <joshuadavidoni@yahoo.com>
2022-03-21 15:08:03 +0100

    test

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const db = knex({
       port : 5432,
       user : 'crhexybuivwcde',
       password : '993be299cd5a24a6818a6086bfb3dbc54f430c5e5ff0c72b11b031ab6dd2eb22',
-      database : 'd3qbrd2ejuq5j'
+      database : 'd3qbrd2ejuq5j',
+      ssl: true
     }
+
 });
 
 const app = express();
